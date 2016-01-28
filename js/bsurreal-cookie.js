4e724b83a38c207bab070c7d556d43777e8ebbe4
interglobalmedia <interglobalmedia@gmail.com>
2016-01-28 06:54:36 -0500

    Make new changes to bsurreal-cookie.js
    Added some new cookie code I am testing out. Only showing pop up to collect name once ie.

--- js/bsurreal-cookie.js
+++ js/bsurreal-cookie.js
@@ -1,22 +1,3 @@
-// Updating the h3 on the game page if the player is a returning user
-/*$(document).ready(function() {
-  // read the cookie
-  var user=getCookie("username");
-  var cookieVal = Cookies.get('username');
-  // if the cookie exists, update the heading
-  if (cookieVal) {
-    $('h3').text('Welcome Back ', + cookieVal + '!');
-  }
-
-  $('.submit-button').on('click', function() {
-    // get value from input.
-    var username = $('.name-field').val();
-    // Update heading
-    $('h3').text('Hi Stranger ', + cookieVal + '!');
-    // Set cookie to save name
-    Cookies.set('username', username);
-  });
-});*/
 // A function to set a cookie
 
 // First, we create a function that stores the name of the visitor in a cookie variable:
@@ -78,3 +59,81 @@ $(document).ready(function() {
     $('h3').text('Welcome Back ' + user + '.' + ' Enjoy the game!');
   }
 });
+
+// Cookie Pop Up only takes place once. The next time they come back, the script will read the cookie, 
+// identify them as a repeat visitor, and NOT open the window again.
+var expDays = 1; // number of days the cookie should last
+
+var page = "still-slider.html";
+var windowprops = "width=300,height=200,location=no,toolbar=no,menubar=no,scrollbars=no,resizable=yes";
+
+function GetCookie (name) {
+var arg = name + "=";
+var alen = arg.length;
+var clen = document.cookie.length;
+var i = 0;
+while (i < clen) {
+var j = i + alen;
+if (document.cookie.substring(i, j) == arg)
+return getCookieVal (j);
+i = document.cookie.indexOf(" ", i) + 1;
+if (i == 0) break;
+}
+return null;
+}
+function SetCookie (name, value) {
+var argv = SetCookie.arguments;
+var argc = SetCookie.arguments.length;
+var expires = (argc > 2) ? argv[2] : null;
+var path = (argc > 3) ? argv[3] : null;
+var domain = (argc > 4) ? argv[4] : null;
+var secure = (argc > 5) ? argv[5] : false;
+document.cookie = name + "=" + escape (value) +
+((expires == null) ? "" : ("; expires=" + expires.toGMTString())) +
+((path == null) ? "" : ("; path=" + path)) +
+((domain == null) ? "" : ("; domain=" + domain)) +
+((secure == true) ? "; secure" : "");
+}
+function DeleteCookie (name) {
+var exp = new Date();
+exp.setTime (exp.getTime() - 1);
+var cval = GetCookie (name);
+document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+}
+var exp = new Date();
+exp.setTime(exp.getTime() + (expDays*24*60*60*1000));
+function amt(){
+var count = GetCookie('count')
+if(count == null) {
+SetCookie('count','1')
+return 1
+}
+else {
+var newcount = parseInt(count) + 1;
+DeleteCookie('count')
+SetCookie('count',newcount,exp)
+return count
+   }
+}
+function getCookieVal(offset) {
+var endstr = document.cookie.indexOf (";", offset);
+if (endstr == -1)
+endstr = document.cookie.length;
+return unescape(document.cookie.substring(offset, endstr));
+}
+
+function checkCount() {
+var count = GetCookie('count');
+if (count == null) {
+count=1;
+SetCookie('count', count, exp);
+
+window.open(page, "", windowprops);
+
+}
+else {
+count++;
+SetCookie('count', count, exp);
+   }
+}
+//  End -->*/

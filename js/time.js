a93272da7b2a419250626079748a6d561cba223a
interglobalmedia <interglobalmedia@gmail.com>
2016-01-29 23:26:06 -0500

    Make new changes to time.js
    Added code for the editable content using jquery.

--- js/time.js
+++ js/time.js
@@ -50,33 +50,15 @@ function getSecs() {
 initStopwatch();
 getSecs();
 
-// Allowing Users to Edit Text Content with HTML5
+// Code for contenteditable/localStorage
 
-function saveEdits() {
+var theContent = $('#edit');
+ 
+$('#save').on('click', function(){
+    var editedContent   = theContent.html();
+    localStorage.newContent = editedContent;
+});
 
-//get the editable element
-var editElem = document.getElementById("edit");
-
-//get the edited element content
-var userVersion = editElem.innerHTML;
-
-//save the content to local storage
-localStorage.userEdits = userVersion;
-
-//write a confirmation to the user
-document.getElementById("update").innerHTML = "Edits saved!";
-
+if(localStorage.getItem('newContent')) {
+    theContent.html(localStorage.getItem('newContent'));
 }
-
-savEdits();
-
-// Check Edits
-
-function checkEdits() {
-
-//find out if the user has previously saved edits
-  if(localStorage.userEdits!=null) {
-    document.getElementById("edit").innerHTML = localStorage.userEdits;
-  }
-}
-checkEdits();
